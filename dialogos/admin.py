11cbe3e9f9ebea56a557635171237b22952f5e20
Ian Schneider <planablediglet@gmail.com>
2012-09-07 10:03:51 -0600

    add submit_date admin filter

--- dialogos/admin.py
+++ dialogos/admin.py
@@ -5,7 +5,7 @@ from dialogos.models import Comment
 
 class CommentAdmin(admin.ModelAdmin):  
     list_display = ["author", "content_type", "public"]
-    list_filter = ["public", "content_type"]
+    list_filter = ["public", "submit_date", "content_type"]
 
 
 admin.site.register(Comment, CommentAdmin)
